4e154b2ea570e4b8d39f92ec964c35d292a32000
Zach <18583032+IronCityCoder@users.noreply.github.com>
2019-06-21 21:15:17 -0500

    Update hasher.py

--- hasher.py
+++ hasher.py
@@ -92,47 +92,52 @@ def runThrough(filePath, pathObj, filetype, md5, sha, ofile):
 	ofile.write(form)
                          
 
+def readFile(filePath, fileType = 'all' hashType = 'all')
+	with open(filePath, "rb") as fp:
+		#All our store is stored as csv anyways so separate on commas
+		for line in fp:
+			newLine = line.split(",")
+			if fileType == 'all' and hashType == 'all':
+				printer(newLine)
+			elif fileType != 'all' and hashType != 'all':
+				if fileType in newLine and hashType in newLine:
+					printer(newLine)
+			elif fileType != 'all':
+				if fileType in newLine:
+					printer(newLine)
+			elif hashType != 'all':
+				if hashType in newLine:
+					printer(newLine)
+					
+def printer(linearr):
+	for i in linearr:
+		print(i)
+		print("-" * 10)
+
+#We will likely need some error messages during testing.		
+def callError():
+	pass
+
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Hasher 2.0 is a file stat program for mobile devices.')
 	group = parser.add_mutually_exclusive_group(required=True)
 	parser.add_argument('path', help="Path: The path files will be read or scanned from.")
 	group.add_argument('-r', action= "store_true", help="Read: Instead of scanning a folder it will read a folder.")
 	group.add_argument('-o', choices=["csv", "txt"], help="Output: Determines the type of output of the scan.")
-	parser.add_argument('--type', help="Type: Will only scan files of a certain type.")
-	parser.add_argument('--hash', help="Hash: Takes in a file that stores a list of hashes and scans matching files.")
+	parser.add_argument('--type', default = "all", help="Type: Will only scan files of a certain type.")
+	parser.add_argument('--hash', default = "all", help="Hash: Takes in a file that stores a list of hashes and scans matching files.")
 	#https://github.com/mac4n6/APOLLO/blob/master/apollo.py
 	#https://docs.python.org/3/library/argparse.html#argparse.ArgumentParser.add_argument
 	args = parser.parse_args()
 
 	global path
-	global filetype
-	global hashing
-
 	path = args.path
-	filetype = args.type
-	hashing = args.hash
 
-	#Read
+	#Because r and o are in a group, they have to pick one.
 	if args.r:
-		#New function
-		#read()
 		print("READING")
+		readFile(path, args.type, args.hash)
 	#Scan
 	elif args.o:
-		#Our existing code.
 		print("SCANNING")
 		scanDir(path, args.o, args.type, args.hash)
-		#scan()
-	#Error
-	else:
-		#New function for printing help out.
-		#callError()
-		print("ERROR!")
-
-
-	#Develop logic flow for what paths we take. 
-	#Two main operations will be read or write, so we can set variables for that. 
-	#If -o then we scan path.
-	#If -r then we open path as rb and print output 
-	#Type and hash will be used by both logic paths for filtering. 
-	#Should be able to have one function for them.
