8b92e48bffcce1111fa84e61ac9ea728fe86d598
Sea <18583032+IronCityCoder@users.noreply.github.com>
2019-07-24 16:04:50 -0400

    Refactored. Passes the read tests. Will need to manually confirm scanning accuracy later.

--- hasher.py
+++ hasher.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-# import all needed modules
+
 import argparse
+import pathlib
 import hashlib
 from sys import argv
 from datetime import datetime as dt
-import pdb
-import pathlib
 
-# In case the computer does not have magic installed
 try:
 		import magic 
 
@@ -18,147 +16,145 @@ except ImportError:
 			Or go to the Github page: \n\
 			- https://github.com/ahupp/python-magic")
 
-'''
-hasher.py is a python script used for mobile device acquisition triage. 
-It lists all files with their simple name, size, sha256 and md5 hashes.
-It also lists the file path and file type for applicable files.
+# Opens a file and appends stored values to a list.
+def open_filter_file(file_name):
+	stack = []
+	with open(file_name, "r") as file:
+		if file_name[-3:] == "csv":
+			# Split by CSV
+			lines = (line.split(",") for line in file)
+			# Remove empty lines
+			lines = (line for line in lines if line)
+		else:
+			# Split by new line 
+			lines = (line.rstrip() for line in file)
+			lines = (line for line in lines if line)ls
 
-arg.py path [-h] [-r] [-o {csv,txt}] [--type TYPE] [--hash HASH] 
+		for line in lines:
+			stack.append(line)
 
-'''
+	return stack
 
-#This function will determine if a list of hashes is passed in and read them.
-#Then it will return a list of hashes.
-def hashFilter(hashType):
-	hashStack = [hashType]
-	acceptableTypes = [".txt", ".csv"]
+# Determine if a file type is provided for parsing hash or filetype filters.
+# Returns a list of either "all" or specific hashes/file types to search for.
+def filter_args(filter_type):
+	stack = [filter_type]
+	acceptable_extensions = [".txt", ".csv"]
 
-	#We go through our hash list and put hashes in a list.
-	if any(x in hashType for x in acceptableTypes):
+	# Determine if filter_type ends with .txt or .csv
+	if any(x in filter_type for x in acceptable_extensions ):
 		try:
-			with open(hashType, "r") as hs:
-				hashStack.clear()
-				#Strip out empty lines.
-				lines = (line.rstrip() for line in hs)
-				lines = (line for line in lines if line)
-				for line in lines:
-					hashStack.append(str(line))
+			# Wipe stack memory and replace it.
+			stack.clear()
+			stack = open_filter_file(filter_type)
 		except:
-			#Use callError to print to error log.
-			print("Error reading hash file.")
-	return hashStack
+			# Use callError to print to error log.
+			call_error("Error reading a filter file.")			
+	return stack
 
-#This function walks through the given directory.
-def scanDir(directory, output, fileType='all', hashType='all') :
-	global ProcessCount
-	global ErrorCount
+# Formats the output of reports.
+def report_formatter(output):
+	date = dt.today()
+	file = f"{date.day}-{date.month}-{date.year}"
+	return f"Scan{file}.{output}"
 
-	ProcessCount = 0
-	ErrorCount = 0
+# Gets the hashes of a file.
+def hashing(file):
+	with open(str(file), "rb") as read_file:
+		file_data = read_file.read() # The bits we will use for hashing.
+
+		md5_hash = hashlib.md5()
+		md5_hash.update(file_data)
+		md5_hash = md5_hash.hexdigest().upper()
+
+		sha_hash = hashlib.sha256()
+		sha_hash.update(file_data)
+		sha_hash = sha_hash.hexdigest ().upper()
+	
+	return md5_hash, sha_hash
+
+# Determine if a filter we want is in the file.
+def logic_tree(stack, info):
+	for count, value in enumerate(stack):
+		for item in info:
+			if item in info:
+				return True
+
+# Checks combinations of booleans
+def combinations(all_hashes, all_files, type_bool, hash_bool):
+	if not(all_hashes and all_files):
+		if not(hash_bool and type_bool):
+			return False
+	elif not(all_hashes and hash_bool):
+		return False
+	elif not(all_files and type_bool):
+		return False	
+	return True
+		
+# Walks through the directory and collects info about each file.
+def scan_dir(directory, file_type = "all", hash_type = "all"):
 	path = pathlib.Path(directory)
 	files = path.rglob("*")
-	date = "{}-{}-{}".format(dt.today().month, dt.today().day, dt.today().year)
-	m = magic.Magic(mime=True)
-	with open("HasherScan{}.{}".format(date, output), "w+") as ofile:
-		for file in files:
-			if file.is_file():
-				#Check file type and hashes.
-				ftype = m.from_file(str(file))
-				with open(str(file), 'rb') as fp:
-					fData = fp.read()
-				hash = hashlib.md5 ()
-				hash256 = hashlib.sha256 ()
-				hash.update(fData)
-				hash256.update(fData)
-				hexMD5 = hash.hexdigest().upper()
-				hexSHA = hash256.hexdigest ().upper()
-				#If a filetype is entered we check it.
-				
-				hashStack = hashFilter(hashType)
-				#If our list is greater than 1, we must have a list of hashes.
-				acceptAllHashes = len(hashStack) == 1
-				#Want to make this its own function later.
-
-				#Have a struct as we filter out stuff? 
-				for i, v in enumerate(hashStack):
-					filebool = fileType in ftype #i.e is png in ftype?
-					hashbool = v in [hexMD5, hexSHA] #do hashes match?
-
-					fbool = fileType != 'all' #file filtering on or off?
-
-					if fbool and not acceptAllHashes and not (filebool and hashbool):
-						continue
-					elif fbool and not filebool:
-						continue
-					elif not(acceptAllHashes) and not hashbool:
-						continue
-					#If we pass filters we will format file info below.
-					runThrough(str(file), file, ftype, hexMD5, hexSHA, ofile)
-			
-#where to handle hash logic 
-def runThrough(filePath, pathObj, filetype, md5, sha, ofile):
-	#Stats about the file are stored here in an object.
-	#For future versions we can have flags to grab specific information.
-	fileStats = pathObj.stat()
+	files = [file for file in files if file.is_file()]
+	file_name = report_formatter(output_type)
 	
-	#Grab the information for each file in a directory.						
-	hHFile = {
-		'hashType': 'MD5',
-		'SHAtype': 'SHA256',
-		'hexMD5': md5,
-		'hexSHA': sha,
-		'size': fileStats.st_size,
-		'FileName': pathObj.name,
-		'FilePath': filePath,
-		'FileType': filetype
-	}
+	# Set up the magic-byte object for determing file type
+	m = magic.Magic(mime=True) # Set mime to False for different output
+	
+	with open(file_name, "w+") as output_file:
+		for file in files:
+			item_type = m.from_file(str(file))
+			md5_hash, sha_hash = hashing(file)
+			
+			# List of what the user wants to filer.
+			# If nothing is supplied, should be "all".
+			filter_hashes = filter_args(hash_type)
+			filter_extensions = filter_args(file_type)
+			
+			# Booleans to determine what we scan
+			all_hashes, all_files = len(filter_hashes) == 1, len(filter_extensions) == 1
+			hash_bool = logic_tree(filter_hashes, [md5_hash, sha_hash])
+			type_bool = logic_tree(filter_extensions, [item_type])
+			# Check combinations of booleans
+			if combinations(all_hashes, all_files, type_bool, hash_bool):
 
-	ofile.write('Filename: {FileName},Filetype: {FileType}, Filepath: {FilePath}, Hashtype: {hashType} - {hexMD5}, SHAtype: {SHAtype} - {hexSHA}, size: {size}\n'.format(**hHFile).replace('    ',''))
-
-def readOption(filePath, fileType = "all", hashType = "all"):
-	path = pathlib.Path(filePath)
-	if path.is_file():
-		with open(filePath, "r") as fp:
-			try:
-				#All our store is stored as csv anyways so separate on commas
-				for line in fp:
-					newLine = line.split(",")
-					#Unlike in the scanning portion, we are working with a list now.
-					#So we have to compare the indexes of a list with our filters.
-					#Set up variables for easier logic.
-					newType = newLine[1]
-					hashStack = hashFilter(hashType)
-					#If our list is greater than 1, we must have a list of hashes.
-					acceptAllHashes = len(hashStack) == 1
-
-					#We go over our list of hashes
-					#and check that the filetype and hashes match up
-					#if allHash is true, we don't even need to check hashes
+				# We either have the types being "all" or they match filters
+				# Clean up the args being passed
+				file_stats = file.stat()
 				
-					for i, v in enumerate(hashStack):
-						check = v in newLine[3], newLine[4]
-						if fileType != 'all' and not acceptAllHashes:
-							if not(fileType in newType and check):
-								continue
-						elif fileType != 'all' and not(fileType in newType):
-							continue
-						elif not acceptAllHashes and not check:
-							continue
-						printer(newLine)
-			except:
-				#Print errors out when reading in data.
-				print("Error reading the file: {}".format(filePath))
-	else:
-		print("{} is not a file.".format(filePath))
+				# Modify output based on output_type
+				output = f"Filename: {file.name}, Filetype: {item_type}, Filepath: {str(file)}, Hashtype: MD5 - {md5_hash}, SHAtype: SHA256 - {sha_hash}, size: {file_stats.st_size}\n".replace('    ','')
+				output_file.write(output)
 
-def printer(linearr):
-	for i, v in enumerate(linearr):
-		print(v.rstrip().replace(' ', ''))
-		if i == len(linearr) - 1:		
+# Will go through a report and print out information.			
+def read_report(path, file_type = "all", hash_type = "all"):
+	path = pathlib.Path(path)
+	if path.is_file():
+		# Setting up booleans for filtering.
+		filter_hashes = filter_args(hash_type)
+		filter_extensions = filter_args(file_type)
+		all_hashes, all_files = len(filter_hashes) == 1, len(filter_extensions) == 1
+		results = open_filter_file(str(path))
+		# Go over each line and check if it matches what we are looking for.
+		for count, value in enumerate(results):
+			#print(value[2])
+			hash_bool = logic_tree(filter_hashes, [value[3], value[4]])
+			type_bool = logic_tree(filter_extensions, [value[1]])
+			if combinations(all_hashes, all_files, type_bool, hash_bool):
+				print_result(value)
+	else:
+		call_error(f"{path} is not a file.")
+
+# Formatting the printed output.
+def print_result(results):
+	for count, value in enumerate(results):
+		print(value.rstrip().replace(' ', ''))
+		if count == len(results) - 1:
 			print("-" * 10)
+				
 
 #We will likely need some error messages during testing.		
-def callError(message):
+def call_error(message):
 	print(f"Error occured: {message}")
 
 if __name__ == "__main__":
@@ -166,19 +162,17 @@ if __name__ == "__main__":
 	group = parser.add_mutually_exclusive_group(required=True)
 	parser.add_argument('path', help="Path: The path files will be read or scanned from.")
 	group.add_argument('-r', action= "store_true", help="Read: Instead of scanning a folder it will read a folder.")
-	group.add_argument('-o', choices=["csv", "txt"], help="Output: Determines the type of output of the scan.")
+	group.add_argument('-o', choices=["csv"], help="Output: Determines the type of output of the scan.")
 	parser.add_argument('--type', default = "all", help="Type: Will only scan files of a certain type.")
 	parser.add_argument('--hash', default = "all", help="Hash: Takes in a file that stores a list of hashes and scans matching files.")
-	#https://github.com/mac4n6/APOLLO/blob/master/apollo.py
-	#https://docs.python.org/3/library/argparse.html#argparse.ArgumentParser.add_argument
 	args = parser.parse_args()
 
-	global path
+	global path #Change this to a local?
 	path = args.path
 
 	#Because r and o are in a group, they have to pick one.
 	if args.r:
-		readOption(path, args.type, str(args.hash))
+		read_report(args.path, args.type, str(args.hash))
 	#Scan
 	elif args.o:
-		scanDir(path, args.o, args.type, str(args.hash))
+		scan_dir(args.path, args.type, str(args.hash))
